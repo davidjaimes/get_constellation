c5b280dbede62b5a5abc21d333c8e1e94091b096
David Jaimes <dj@davidjaimes.com>
2021-11-27 21:00:50 -0800

    finish calc date

--- get_constellation.py
+++ get_constellation.py
@@ -5,14 +5,34 @@ import pandas as pd
 from astropy.time import Time
 
 
-# Create datetimes array.
-times = [f'{x:04d}-06-16' for x in range(0, 5001, 500)]
+# Gregorian Days of the Year
+def calc_date(year):
+    dates = []
+    for x in range(1, 13, 1):
+        for y in range(1, 32, 1):
+            if x == 2:
+                if year % 4 != 0:
+                    if y > 28: continue
+                else:
+                    if y > 29: continue
+                dates.append(f'{year:04d}-{x:02d}-{y:02d}')
 
-# Prepare Data Frame
-t = Time(times)
-df = pd.DataFrame({
-    'Date': t,
-    'Zign': get_sun(t).get_constellation()
-    })
+            elif x in [4, 6, 9, 11]:
+                if y > 30: continue
+                dates.append(f'{year:04d}-{x:02d}-{y:02d}')
 
-print(df)
+            else:
+                dates.append(f'{year:04d}-{x:02d}-{y:02d}')
+    return dates
+
+# Create DataFrame
+dates = calc_date(1986)
+df = pd.DataFrame({'Date': dates})
+times = Time(dates)
+df['Sign'] = get_sun(times).get_constellation()
+df = df.set_index('Sign')
+
+# Find min and max fo reach sign.
+signs = ['Aquarius', 'Pisces', 'Aries', 'Taurus', ]
+
+print(df.loc['Libra'].max())
