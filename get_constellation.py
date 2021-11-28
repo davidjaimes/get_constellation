38f4e34d81e664d02ec68265970df00b0b070664
David Jaimes <dj@davidjaimes.com>
2021-11-27 19:15:19 -0800

    don't use pandas timeseries index

--- get_constellation.py
+++ get_constellation.py
@@ -2,23 +2,17 @@ from astropy.coordinates import get_sun, get_constellation
 from astropy.timeseries import TimeSeries
 import numpy as np
 import pandas as pd
+from astropy.time import Time
 
 
 # Create datetimes array.
-dates = [f'{x+1678}/12/16' for x in range(584)]
+times = [f'{x:04d}-06-16' for x in range(0, 5001, 500)]
 
 # Prepare Data Frame
-df = pd.DataFrame()
-df['a'] = np.arange(1, len(dates) + 1, 1)
-df.set_index(pd.DatetimeIndex(dates), inplace=True)
-
-# Turn from pandas to astropy time.
-ts = TimeSeries.from_pandas(df)
-
-# New DF
-ndf = pd.DataFrame({
-    'Date': ts['time'],
-    'Zign': get_sun(ts['time']).get_constellation()
+t = Time(times)
+df = pd.DataFrame({
+    'Date': t,
+    'Zign': get_sun(t).get_constellation()
     })
 
-print(ndf)
+print(df)
